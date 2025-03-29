892a1b477bc304b69eabb2fe1d24be377a6a2e57
LieOnLion <lieonlion4@gmail.com>
2025-03-29 10:59:21 +0000

    innit commit

---
+++ app.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+
+    rollupConfig: {
+      external: ["node:async_hooks"]
+    }
+  },
+  
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
