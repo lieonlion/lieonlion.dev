700dcd72418bf9cadb2912ab7e094c92e59a12b4
LieOnLion <lieonlion4@gmail.com>
2025-03-29 12:05:15 +0000

    yay, meta data

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,17 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
 import "./app.css";
+import { MetaProvider, Title } from "@solidjs/meta";
 
 export default function App() {
   return (
     <Router
-      root={props => (
-        <>
+      root={(props) => (
+        <MetaProvider>
+          <Title>SolidStart - Basic</Title>
           <Nav />
           <Suspense>{props.children}</Suspense>
-        </>
+        </MetaProvider>
       )}
     >
       <FileRoutes />
